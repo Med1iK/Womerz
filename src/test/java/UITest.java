f5eb3d46c0e39e149d65395256b8d0b34b15b655
Med1ik <illyakochubey@gmail.com>
2016-09-28 18:31:04 +0300

    Take cookie from authorization method

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,25 +1,30 @@
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import uisteps.AuthorizationPageUser;
+
+import java.util.Set;
 
 @RunWith(SerenityRunner.class)
 public class UITest {
 
+
     @Managed(uniqueSession = true)
     public WebDriver webDriver;
 
-    @Before
+/*    @Before
     public void setUp(){
         succsessfullyLogin();
-    }
+    }*/
 
     @Steps
     public AuthorizationPageUser authorizationPageUser;
+
+  //  private static String JSESSIONID;
 
     @Test
     public void succsessfullyLogin(){
@@ -28,6 +33,9 @@ public class UITest {
         authorizationPageUser.EnterUserPassword("*********");
         authorizationPageUser.ClickOnLoginButton();
       //  authorizationPageUser.EnterUserSecondPassword("*********");
-     //   authorizationPageUser.ClickOnContinueButton();
+      //  authorizationPageUser.ClickOnContinueButton();
+        Set<Cookie> ck = webDriver.manage().getCookies();
+        System.out.println(ck);
     }
+
 }
